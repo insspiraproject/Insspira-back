c0be1b22c335f1848a947c8225b40fc76112267c
GAgustínFranco <agustingalvez0901@gmail.com>
2025-09-18 12:18:53 -0300

    mercado pago

--- src/mercadopago/mercadopago.module.ts
+++ src/mercadopago/mercadopago.module.ts
@@ -2,10 +2,15 @@ import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MercadoPagoService } from './mercadopago.service';
 import { MercadoPagoController } from './mercadopago.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from '../payments/payment.entity';
 
 @Global()
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([Payment])
+  ],
   controllers: [MercadoPagoController],
   providers: [MercadoPagoService],
   exports: [MercadoPagoService],
