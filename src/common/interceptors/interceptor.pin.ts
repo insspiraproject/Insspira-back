1fc7b6b5f5759c3072503963f782f5656080299d
GAgustínFranco <agustingalvez0901@gmail.com>
2025-09-29 19:18:57 -0300

    mp

--- src/common/interceptors/interceptor.pin.ts
+++ src/common/interceptors/interceptor.pin.ts
@@ -1,6 +1,6 @@
 import { CallHandler, ExecutionContext, ForbiddenException, NestInterceptor } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { mergeMap, Observable, tap } from "rxjs";
+import { lastValueFrom, mergeMap, Observable, tap } from "rxjs";
 import { ActionType } from "src/pins.enum";
 import { SubStatus } from "src/status.enum";
 import { Sub } from "src/subscriptions/subscription.entity";
@@ -16,7 +16,7 @@ export class LimitInterceptor implements NestInterceptor {
     ) {}
 
 
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
+    async intercept(context: ExecutionContext, next: CallHandler<any>): Promise<any> {
         
     const req = context.switchToHttp().getRequest();
     const user = req.user;
@@ -24,20 +24,18 @@ export class LimitInterceptor implements NestInterceptor {
     const sub = req.subscription;
 
 
-    return next.handle().pipe(
-        mergeMap(async(data)=>{
-             const activate = sub ?? await this.subRepo.findOne({
-          where: { user: { id: user.sub }, status: In([SubStatus.ENABLED, SubStatus.ACTIVE]) }
+    const activate = sub ?? await this.subRepo.findOne({
+        where: { user: { id: user.sub }, status: In([SubStatus.ENABLED, SubStatus.ACTIVE]) }
         });
         if(!activate) throw new ForbiddenException("You don't have any subscription")
 
-        // Validar según acción
+
         switch (action) {
             case ActionType.POST:
                 if (activate.dailyPosts >= 3) {
                     throw new ForbiddenException('You have reached the daily limit of 3 posts with the free plan');
                 }
-                activate.dailyPosts++   
+                activate.dailyPosts++
             break;
 
             case ActionType.LIKE:
@@ -45,7 +43,7 @@ export class LimitInterceptor implements NestInterceptor {
                     throw new ForbiddenException('You have reached the daily limit of 5 likes with the free plan');
                 }
                 activate.dailyLikes++;
-             
+
             break;
 
             case ActionType.SAVE:
@@ -53,7 +51,7 @@ export class LimitInterceptor implements NestInterceptor {
                     throw new ForbiddenException('You have reached the daily limit of 2 saves with the free plan');
                 }
                 activate.dailySaves++;
-            
+
             break;
 
             case ActionType.COMMENT:
@@ -61,18 +59,12 @@ export class LimitInterceptor implements NestInterceptor {
                     throw new ForbiddenException('You have reached the daily limit of 3 comments with the free plan');
                 }
                 activate.dailyComments++;
-            
+
             break;
         }
-        
+
         await this.subRepo.save(activate);
-        return data
-
-        })
-    )
-
-
-
-    }
+        return await lastValueFrom(next.handle())
+    } 
     
 }
