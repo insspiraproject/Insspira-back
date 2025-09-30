c4c4db134edc2c19c2f80b7f8b3578ed8f6b0575
GAgustínFranco <agustingalvez0901@gmail.com>
2025-09-30 00:01:15 -0300

    mp

--- src/plans/seeder.plan.ts
+++ src/plans/seeder.plan.ts
@@ -9,14 +9,14 @@ export const PlanFreeMonthlyAnnual = [  {
     },
     {
         name: "Monthly Plan",
-        price: 0,
+        price: 0.10,
         type: "monthly",
         currency: "ARS",  
         features: "Pin creation limit. Limit of likes per post, saves, and comments."
     },
     {
         name: "Annual Plan",
-        price: 0,
+        price: 0.30,
         type: "annual",
         currency: "ARS",  
         features: "Pin creation limit. Limit of likes per post, saves, and comments."
