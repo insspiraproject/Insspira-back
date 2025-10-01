4fb355564bfbe2b6b0b40216d22fcf1f1c5239e0
GAgustínFranco <agustingalvez0901@gmail.com>
2025-10-01 11:17:35 -0300

    maintenance

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -9,14 +9,14 @@ export class LoginUserDto {
   email: string;
 
 
-  // @ApiPropertyOptional({ minLength: 8 })
-  // @IsString()
-  // @IsOptional() 
-  // @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  // @MaxLength(100, { message: 'Password must not exceed 100 characters' })
-  // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/, {
-  //           message: 'The password must contain at least one lowercase letter, one uppercase letter, one number, and one special character (!@#$%^&*).',
-  //       })
+  @ApiPropertyOptional({ minLength: 8 })
+  @IsString()
+  @IsOptional() 
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(100, { message: 'Password must not exceed 100 characters' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/, {
+            message: 'The password must contain at least one lowercase letter, one uppercase letter, one number, and one special character (!@#$%^&*).',
+        })
         password?: string;
 
 
